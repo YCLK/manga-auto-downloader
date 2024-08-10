6e6f3dd9da14b8883d8e538f90077e4004726d41
YCLK <youchan8721@gmail.com>
2024-08-10 12:44:59 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -7,90 +7,110 @@ import os
 print('Manga Auto Downloader (Ver. 1.1)    제작: YCLK')
 time.sleep(3)
 
-# 확장 프로그램 좌표 입력
-time_cnt = 5
+# 저장 안내
+answer = input("저장한 설정 값을 불러오시겠습니까? (Y/N):")
 
-while time_cnt:
-    print(str(time_cnt) + '초 후에 Image Downloader 확장 프로그램에 마우스를 위치하세요')
-    time_cnt -= 1
-    time.sleep(1)
+if answer == "N" or "n" : 
+    # 스크롤바 좌표 입력
+    time_cnt = 5
 
-print(pyautogui.position())
+    while time_cnt:
+        print(str(time_cnt) + '초 후에 스크롤 바 최상단에 마우스를 위치하세요')
+        time_cnt -= 1
+        time.sleep(1)
 
-ExtentionX = int(input('x좌표를 입력해 주세요: '))
-ExtentionY = int(input('y좌표를 입력해 주세요: '))
-os.system('cls')
+    ScrollTopX, ScrollTopY = pyautogui.position()
+    os.system('cls')
 
-# Select all 체크박스 좌표 입력
-time_cnt = 5
+    # 스크롤바 좌표 입력
+    time_cnt = 5
 
-while time_cnt:
-    print(str(time_cnt) + '초 후에 Select all 체크박스에 마우스를 위치하세요')
-    time_cnt -= 1
-    time.sleep(1)
+    while time_cnt:
+        print(str(time_cnt) + '초 후에 스크롤 바 하단에 마우스를 위치하세요')
+        time_cnt -= 1
+        time.sleep(1)
 
-print(pyautogui.position())
+    ScrollBottomX, ScrollBottomY = pyautogui.position()
+    os.system('cls')
 
-SelectX = int(input('x좌표를 입력해 주세요: '))
-SelectY = int(input('y좌표를 입력해 주세요: '))
-os.system('cls')
+    # 확장 프로그램 좌표 입력
+    time_cnt = 5
 
-# 폴더 이름 지정 좌표 입력
-time_cnt = 5
+    while time_cnt:
+        print(str(time_cnt) + '초 후에 Image Downloader 확장 프로그램에 마우스를 위치하세요')
+        time_cnt -= 1
+        time.sleep(1)
 
-while time_cnt:
-    print(str(time_cnt) + '초 후에 Save to subfolder 란에 마우스를 위치하세요')
-    time_cnt -= 1
-    time.sleep(1)
+    ExtentionX, ExtentionY = pyautogui.position()
+    os.system('cls')
 
-print(pyautogui.position())
+    # Select all 체크박스 좌표 입력
+    time_cnt = 5
 
-FolderX = int(input('x좌표를 입력해 주세요: '))
-FolderY = int(input('y좌표를 입력해 주세요: '))
-os.system('cls')
+    while time_cnt:
+        print(str(time_cnt) + '초 후에 Select all 체크박스에 마우스를 위치하세요')
+        time_cnt -= 1
+        time.sleep(1)
 
-# 다운로드 버튼 좌표 입력
-time_cnt = 5
+    SelectX, SelectY = pyautogui.position()
+    os.system('cls')
 
-while time_cnt:
-    print(str(time_cnt) + '초 후에 Download 버튼 위에 마우스를 위치하세요')
-    time_cnt -= 1
-    time.sleep(1)
+    # 폴더 이름 지정 좌표 입력
+    time_cnt = 5
 
-print(pyautogui.position())
+    while time_cnt:
+        print(str(time_cnt) + '초 후에 Save to subfolder 란에 마우스를 위치하세요')
+        time_cnt -= 1
+        time.sleep(1)
 
-DownloadX = int(input('x좌표를 입력해 주세요: '))
-DownloadY = int(input('y좌표를 입력해 주세요: '))
-os.system('cls')
+    FolderX, FolderY = pyautogui.position()
+    os.system('cls')
 
-# 다음화 버튼 좌표 입력
-time_cnt = 5
+    # 다운로드 버튼 좌표 입력
+    time_cnt = 5
 
-while time_cnt:
-    print(str(time_cnt) + '초 후에 플랫폼 다음화 버튼 위에 마우스를 위치하세요')
-    time_cnt -= 1
-    time.sleep(1)
+    while time_cnt:
+        print(str(time_cnt) + '초 후에 Download 버튼 위에 마우스를 위치하세요')
+        time_cnt -= 1
+        time.sleep(1)
 
-print(pyautogui.position())
+    DownloadX, DownloadY = pyautogui.position()
+    os.system('cls')
 
-NextX = int(input('x좌표를 입력해 주세요: '))
-NextY = int(input('y좌표를 입력해 주세요: '))
-os.system('cls')
+    # 다음화 버튼 좌표 입력
+    time_cnt = 5
+
+    while time_cnt:
+        print(str(time_cnt) + '초 후에 플랫폼 다음화 버튼 위에 마우스를 위치하세요')
+        time_cnt -= 1
+        time.sleep(1)
+
+    NextX, NextY = pyautogui.position()
+    os.system('cls')
+
+    # 이미지 로드 시간 입력
+    delayLoadTime = int(input('이미지 로드 대기 시간을 지정해 주세요 (사진이 많을 수록 길게 지정, 10초 권장): '))
+
+    # 다운로드 대기 시간 입력
+    delayDownloadTime = int(input('이미지 다운로드 대기 시간을 지정해 주세요 (사진이 많을 수록 길게 지정, 20초 권장): '))
+else : 
+    loaded_variables = {}
+    with open("variables.txt", "r") as file:
+        for line in file:
+            key, value = line.strip().split('=')
+            # 문자열을 정수로 변환하거나 그대로 두기 (예제에서는 정수만)
+            if value.isdigit():
+                value = int(value)
+            loaded_variables[key] = value
 
 # 만화 제목 입력
 mangaTitle = input('저장할 작품의 이름을 지정해 주세요: ')
 
-# 이미지 로드 시간 입력
-delayLoadTime = int(input('이미지 로드 대기 시간을 지정해 주세요 (사진이 많을 수록 길게 지정 10초 정도): '))
-
-# 다운로드 대기 시간 입력
-delayDownloadTime = int(input('이미지 다운로드 대기 시간을 지정해 주세요 (사진이 많을 수록 길게 지정 20초 정도): '))
-
 # 시작하는 화 입력
-numb = int(input('시작하는 화를 지정해 주세요: '))
+chapterNumber = int(input('시작하는 화를 지정해 주세요: '))
 
 #끝나는 화 입력
-num1 = int(input('끝나는 화를 지정해 주세요: '))
+finishChapter = int(input('끝나는 화를 지정해 주세요: '))
 os.system('cls')
 
 # 다운로드 시간 표시
@@ -105,8 +125,11 @@ os.system('cls')
 # num변수 선언
 num = 0
 
-while num < num1:
+while num < finishChapter:
     num += 1
+
+    pyautogui.moveTo(ScrollTopX, ScrollTopY, duration=0.3)
+    pyautogui.dragTo(ScrollBottomX, ScrollBottomY, duration=5)
 
     # 확장 프로그램 클릭
     pyautogui.moveTo(ExtentionX, ExtentionY, duration=0.3)
@@ -141,8 +164,8 @@ while num < num1:
     pyperclip.copy(mangaTitle)
     pyautogui.hotkey('ctrl', 'v')
     pyautogui.write('-')
-    pyautogui.write(str(numb))
-    numb += 1
+    pyautogui.write(str(chapterNumber))
+    chapterNumber += 1
     pyperclip.copy('화')
     pyautogui.hotkey('ctrl', 'v')
 
